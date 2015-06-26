d789456a8ee117671b042f623cf2dc3e8823b9b9
Syfaro <syfaro@foxpaw.in>
2015-06-25 23:44:14 -0500

    load self when created

--- bot.go
+++ bot.go
@@ -3,11 +3,21 @@ package tgbotapi
 type BotApi struct {
 	Token   string      `json:"token"`
 	Debug   bool        `json:"debug"`
+	Self    User        `json:"-"`
 	Updates chan Update `json:"-"`
 }
 
-func NewBotApi(token string) *BotApi {
-	return &BotApi{
+func NewBotApi(token string) (*BotApi, error) {
+	bot := &BotApi{
 		Token: token,
 	}
+
+	self, err := bot.GetMe()
+	if err != nil {
+		return BotApi{}, err
+	}
+
+	bot.Self = self
+
+	return bot
 }
