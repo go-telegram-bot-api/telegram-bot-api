d1358d12aaa4207728e8a60aa1c62b670611f9c3
Sergey Kamardin <gobwas@gmail.com>
2015-09-13 21:56:43 +0300

    Update webhook.go
    Why there is a hardcoded `bot.Token`? =)

--- webhook.go
+++ webhook.go
@@ -9,10 +9,10 @@ import (
 // ListenForWebhook registers a http handler for a webhook.
 // Useful for Google App Engine or other places where you cannot
 // use a normal update chan.
-func (bot *BotAPI) ListenForWebhook() {
+func (bot *BotAPI) ListenForWebhook(config WebhookConfig) {
 	bot.Updates = make(chan Update, 100)
 
-	http.HandleFunc("/"+bot.Token, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/"+config.Url.Path, func(w http.ResponseWriter, r *http.Request) {
 		bytes, _ := ioutil.ReadAll(r.Body)
 
 		var update Update
