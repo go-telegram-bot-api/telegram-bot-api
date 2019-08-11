49a467b7c416fd06d6de7f67549ac42d5339e78b
rozha <il.novikov@gmail.com>
2019-08-11 13:20:40 +0300

    Introduce NewOneTimeReplyKeyboard() helper function
    (cherry picked from commit b478ff9669daca239e94cf0659e7c941a5071a6c)

--- helpers.go
+++ helpers.go
@@ -761,6 +761,13 @@ func NewReplyKeyboard(rows ...[]KeyboardButton) ReplyKeyboardMarkup {
 	}
 }
 
+// NewOneTimeReplyKeyboard creates a new one time keyboard using NewReplyKeyboard()
+func NewOneTimeReplyKeyboard(rows ...[]KeyboardButton) ReplyKeyboardMarkup {
+  markup := NewReplyKeyboard(rows...)
+  markup.OneTimeKeyboard = true
+  return markup
+}
+
 // NewInlineKeyboardButtonData creates an inline keyboard button with text
 // and data for a callback.
 func NewInlineKeyboardButtonData(text, data string) InlineKeyboardButton {
