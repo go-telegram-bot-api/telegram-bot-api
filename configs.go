0b0783f15d6de8fb80bbe01a4b96fc1bd396cc5e
maxwell <maxwell_jun@163.com>
2020-02-01 14:56:43 +0000

    permissions sp2

--- configs.go
+++ configs.go
@@ -1035,14 +1035,14 @@ type PromoteChatMemberConfig struct {
 	CanPromoteMembers  *bool
 }
 type ChatPermissions struct {
-	CanSendMessages       *bool
-	CanSendMediaMessages  *bool
-	CanSendPolls          *bool
-	CanSendOtherMessages  *bool
-	CanAddWebPagePreviews *bool
-	CanChangeInfo         *bool
-	CanInviteUsers        *bool
-	CanPinMessages        *bool
+	CanSendMessages       *bool `json:"can_send_messages"`
+	CanSendMediaMessages  *bool `json:"can_send_media_messages"`
+	CanSendPolls          *bool `json:"can_send_polls"`
+	CanSendOtherMessages  *bool `json:"can_send_other_messages"`
+	CanAddWebPagePreviews *bool `json:"can_add_web_page_previews"`
+	CanChangeInfo         *bool `json:"can_change_info"`
+	CanInviteUsers        *bool `json:"can_invite_users"`
+	CanPinMessages        *bool `json:"can_pin_messages"`
 }
 
 // SetChatPermissionsConfig contains fields to restrict members of chat
