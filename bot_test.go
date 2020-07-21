5c5e96de34ba4bcb0b1d1e9dd757559577c71e47
Jiayu Yi <yijiayu@gmail.com>
2020-07-21 16:37:08 +0800

    Change assertion failure message

--- bot_test.go
+++ bot_test.go
@@ -498,7 +498,7 @@ func TestSetWebhookWithoutCert(t *testing.T) {
 		t.Error(err)
 	}
 	if info.MaxConnections == 0 {
-		t.Errorf("wanted max connections to be greater than 0")
+		t.Errorf("Expected maximum connections to be greater than 0")
 	}
 	if info.LastErrorDate != 0 {
 		t.Errorf("[Telegram callback failed]%s", info.LastErrorMessage)
