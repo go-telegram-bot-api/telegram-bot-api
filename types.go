b415a10d319c51bc0998a07a118f42b27cab2389
Abhai Kollara <abhai.dilip@gmail.com>
2020-10-03 21:47:40 +0530

    Add LoginURL to InlineKeyboardButton

--- types.go
+++ types.go
@@ -542,11 +542,20 @@ type InlineKeyboardMarkup struct {
 type InlineKeyboardButton struct {
 	Text                         string        `json:"text"`
 	URL                          *string       `json:"url,omitempty"`                              // optional
+	LoginURL                     *LoginURL 	   `json:"login_url"`
 	CallbackData                 *string       `json:"callback_data,omitempty"`                    // optional
 	SwitchInlineQuery            *string       `json:"switch_inline_query,omitempty"`              // optional
 	SwitchInlineQueryCurrentChat *string       `json:"switch_inline_query_current_chat,omitempty"` // optional
 	CallbackGame                 *CallbackGame `json:"callback_game,omitempty"`                    // optional
 	Pay                          bool          `json:"pay,omitempty"`                              // optional
+}
+
+// LoginURL represents a parameter of the inline keyboard button used to automatically authorize a user
+type LoginURL struct {
+	URL                string `json:"url"`
+	ForwardText        string `json:"forward_text"`
+	BotUsername        string `json:"bot_username"`
+	RequestWriteAccess bool   `json:"request_write_access"`
 }
 
 // CallbackQuery is data sent when a keyboard button with callback data
