f54d1e81cd45f0b0a19d34c8009ced58056cebdd
Abhai Kollara <abhai.dilip@gmail.com>
2020-10-03 19:18:35 +0530

    Update ChatPhoto fields to latest api

--- types.go
+++ types.go
@@ -90,8 +90,10 @@ type GroupChat struct {
 
 // ChatPhoto represents a chat photo.
 type ChatPhoto struct {
-	SmallFileID string `json:"small_file_id"`
-	BigFileID   string `json:"big_file_id"`
+	SmallFileID       string `json:"small_file_id"`
+	SmallFileUniqueID string `json:"small_file_unique_id"`
+	BigFileID         string `json:"big_file_id"`
+	BigFileUniqueID   string `json:"big_file_unique_id"`
 }
 
 // Chat contains information about the place a message was sent.
