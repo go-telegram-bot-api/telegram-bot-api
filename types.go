3a4c912f47110c166edd3c01f0f3b9b4a1c88814
Abhai Kollara <abhai.dilip@gmail.com>
2020-10-03 20:14:37 +0530

    Add 'file_unique_id' field to 'PhotoSize' object

--- types.go
+++ types.go
@@ -349,6 +349,7 @@ func (e MessageEntity) IsTextMention() bool {
 // PhotoSize contains information about photos.
 type PhotoSize struct {
 	FileID   string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
 	Width    int    `json:"width"`
 	Height   int    `json:"height"`
 	FileSize int    `json:"file_size"` // optional
