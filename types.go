5875a46cdedea21de70ec6f5b4d4825bb1fe6f6e
Ilya Kaznacheev <ilya@kaznacheev.me>
2020-10-24 17:00:46 +0300

    Add game type documentation

--- types.go
+++ types.go
@@ -1019,12 +1019,27 @@ func (chat ChatMember) WasKicked() bool { return chat.Status == "kicked" }
 
 // Game is a game within Telegram.
 type Game struct {
-	Title        string          `json:"title"`
-	Description  string          `json:"description"`
-	Photo        []PhotoSize     `json:"photo"`
-	Text         string          `json:"text"`
+	// Title of the game
+	Title string `json:"title"`
+	// Description of the game
+	Description string `json:"description"`
+	// Photo that will be displayed in the game message in chats.
+	Photo []PhotoSize `json:"photo"`
+	// Text a brief description of the game or high scores included in the game message.
+	// Can be automatically edited to include current high scores for the game
+	// when the bot calls setGameScore, or manually edited using editMessageText. 0-4096 characters.
+	//
+	// optional
+	Text string `json:"text"`
+	// TextEntities special entities that appear in text, such as usernames, URLs, bot commands, etc.
+	//
+	// optional
 	TextEntities []MessageEntity `json:"text_entities"`
-	Animation    Animation       `json:"animation"`
+	// Animation animation that will be displayed in the game message in chats.
+	// Upload via BotFather (https://t.me/botfather).
+	//
+	// optional
+	Animation Animation `json:"animation"`
 }
 
 // Animation is a GIF animation demonstrating the game.
