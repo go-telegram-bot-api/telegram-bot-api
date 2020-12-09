3c75c85c716eb9ca5c4818d931825ed590b679b9
Ilia Choly <ilia.choly@gmail.com>
2020-12-09 15:44:27 -0500

    Add SIV to import paths to support modules

--- types_test.go
+++ types_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func TestUserStringWith(t *testing.T) {
