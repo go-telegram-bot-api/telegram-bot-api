f34cdd675862664bd92aab94894d16bb948f924e
romanitalian <romanitalian.net@gmail.com>
2021-02-13 23:18:45 +0300

    update go module for the last tag - v5; set v5 in tests

--- helpers_test.go
+++ helpers_test.go
@@ -3,7 +3,7 @@ package tgbotapi_test
 import (
 	"testing"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func TestNewInlineQueryResultArticle(t *testing.T) {
